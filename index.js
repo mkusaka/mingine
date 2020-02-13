3aeca083d25078066a89c83eab2adae278299e6d
mkusaka <hinoshita1992@gmail.com>
2020-02-13 21:46:28 +0900

    simlify

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const Arborist = require('@npmcli/arborist')
-const semver = require('semver')
-
 const data = {} // initialize data we'll return
 
 async function mingine (options) {
@@ -25,50 +23,9 @@ async function mingine (options) {
 function packageMapHandler (value, key, map) {
   const symbols = Object.getOwnPropertySymbols(value)
   const children = map.get(key)
-  const enginesPropertyInPacakgeJSON = children[symbols[0]].engines // only extract the engines property
+  const enginesPropertyInPacakgeJSON = children[symbols[0]].version // only extract the engines property
   const namePropertyInPacakgeJSON = children[symbols[0]].name // only extract the name property
-
-  if (enginesPropertyInPacakgeJSON !== undefined) {
-    const presentEngines = Object.keys(enginesPropertyInPacakgeJSON)
-
-    presentEngines.forEach(engine => { // in an unopinionated way, iterate over the values for every single engine and build our API
-      const version = semverify(enginesPropertyInPacakgeJSON[engine]) // semverify normalizes semver ranges for our use case
-
-      if (data[engine] === undefined) {
-        data[engine] = {}
-      }
-
-      if (data[engine].versions === undefined) {
-        data[engine].versions = {}
-      }
-
-      if (data[engine].versions[version] === undefined) {
-        data[engine].versions[version] = []
-      }
-
-      if (data[engine].minimum === undefined) {
-        data[engine].minimum = version
-      }
-
-      if (semver.valid(semver.coerce(version)) !== null) {
-        if (semver.lt(data[engine].minimum, version)) {
-          data[engine].minimum = version
-        }
-      }
-
-      data[engine].versions[version].push(namePropertyInPacakgeJSON)
-    })
-  }
-}
-
-function semverify (versionlike) {
-  let cleaned = semver.valid(semver.coerce(versionlike))
-
-  if (cleaned === null) {
-    cleaned = versionlike
-  }
-
-  return cleaned
+  console.log(namePropertyInPacakgeJSON, enginesPropertyInPacakgeJSON);
 }
 
 module.exports = mingine
